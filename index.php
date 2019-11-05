c956fa7db7c50229b6a399aa8f6dd0f3bf5e23db
Denus934 <56293616+Denus934@users.noreply.github.com>
2019-11-05 11:29:54 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<title>РК2</title>
+	<charset meta="utf-8">
+	</head>
+	<form action ="" method="get">
+	<body>
+	   <h1> Chernyishov D.D. IU4-12B</h1>
+	   <h2> Задание 1 </h2>
+	   
+	<?php
+	echo "<table border = 1px>";
+	 for ($i=0; $i < 101; $i++) {
+	echo "<tr><td><value="$i"></td></tr>;
+	}
+	
+	echo "</table>"
+	?>
+	
+	
+	 <h2> Task 2 </h2>
+	 <form action ="" method="get">
+	 <?php
+	 $i=
+	 $s=
+	 $summ= $i + $s;
+	 $raz= $i - $s;
+	 $pr= $i * $s;
+	 $del= $i / $s;
+	 if () {
+	 echo $summ
+	 } ;
+	 if () {
+	 echo $raz
+	 } ;
+	 if () {
+	 echo $pr 
+	 } ;
+	if () {
+	echo $del
+	} ;
+	 ?>
+	</body>
+</html>	
+	
